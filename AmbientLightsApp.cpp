6ab8ccf13746b5c3884e2aab72bf83c9bdf6acbb
PHolmes1011 <61152948+PHolmes1011@users.noreply.github.com>
2021-10-22 11:30:24 +0100

    Renamed LitColumnsApp
    Renamed LitColumnsApp to AmbientLightApp, just out of personal preference.

--- AmbientLightsApp.cpp
+++ AmbientLightsApp.cpp
@@ -1,5 +1,5 @@
 //***************************************************************************************
-// LitColumnsApp.cpp by Frank Luna (C) 2015 All Rights Reserved.
+// AmbientLightApp.cpp by Frank Luna (C) 2015 All Rights Reserved.
 //***************************************************************************************
 
 #include "../../Common/d3dApp.h"
@@ -51,13 +51,13 @@ struct RenderItem
     int BaseVertexLocation = 0;
 };
 
-class LitColumnsApp : public D3DApp
+class AmbientLightApp : public D3DApp
 {
 public:
-    LitColumnsApp(HINSTANCE hInstance);
-    LitColumnsApp(const LitColumnsApp& rhs) = delete;
-    LitColumnsApp& operator=(const LitColumnsApp& rhs) = delete;
-    ~LitColumnsApp();
+    AmbientLightApp(HINSTANCE hInstance);
+    AmbientLightApp(const AmbientLightApp& rhs) = delete;
+    AmbientLightApp& operator=(const AmbientLightApp& rhs) = delete;
+    ~AmbientLightApp();
 
     virtual bool Initialize()override;
 
@@ -137,7 +137,7 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE prevInstance,
 
     try
     {
-        LitColumnsApp theApp(hInstance);
+        AmbientLightApp theApp(hInstance);
         if(!theApp.Initialize())
             return 0;
 
@@ -150,18 +150,18 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE prevInstance,
     }
 }
 
-LitColumnsApp::LitColumnsApp(HINSTANCE hInstance)
+AmbientLightApp::AmbientLightApp(HINSTANCE hInstance)
     : D3DApp(hInstance)
 {
 }
 
-LitColumnsApp::~LitColumnsApp()
+AmbientLightApp::~AmbientLightApp()
 {
     if(md3dDevice != nullptr)
         FlushCommandQueue();
 }
 
-bool LitColumnsApp::Initialize()
+bool AmbientLightApp::Initialize()
 {
     if(!D3DApp::Initialize())
         return false;
@@ -193,7 +193,7 @@ bool LitColumnsApp::Initialize()
     return true;
 }
  
-void LitColumnsApp::OnResize()
+void AmbientLightApp::OnResize()
 {
     D3DApp::OnResize();
 
@@ -202,7 +202,7 @@ void LitColumnsApp::OnResize()
     XMStoreFloat4x4(&mProj, P);
 }
 
-void LitColumnsApp::Update(const GameTimer& gt)
+void AmbientLightApp::Update(const GameTimer& gt)
 {
     OnKeyboardInput(gt);
 	UpdateCamera(gt);
@@ -227,7 +227,7 @@ void LitColumnsApp::Update(const GameTimer& gt)
 	UpdateMainPassCB(gt);
 }
 
-void LitColumnsApp::Draw(const GameTimer& gt)
+void AmbientLightApp::Draw(const GameTimer& gt)
 {
     auto cmdListAlloc = mCurrFrameResource->CmdListAlloc;
 
@@ -284,7 +284,7 @@ void LitColumnsApp::Draw(const GameTimer& gt)
     mCommandQueue->Signal(mFence.Get(), mCurrentFence);
 }
 
-void LitColumnsApp::OnMouseDown(WPARAM btnState, int x, int y)
+void AmbientLightApp::OnMouseDown(WPARAM btnState, int x, int y)
 {
     mLastMousePos.x = x;
     mLastMousePos.y = y;
@@ -292,12 +292,12 @@ void LitColumnsApp::OnMouseDown(WPARAM btnState, int x, int y)
     SetCapture(mhMainWnd);
 }
 
-void LitColumnsApp::OnMouseUp(WPARAM btnState, int x, int y)
+void AmbientLightApp::OnMouseUp(WPARAM btnState, int x, int y)
 {
     ReleaseCapture();
 }
 
-void LitColumnsApp::OnMouseMove(WPARAM btnState, int x, int y)
+void AmbientLightApp::OnMouseMove(WPARAM btnState, int x, int y)
 {
     if((btnState & MK_LBUTTON) != 0)
     {
@@ -329,11 +329,11 @@ void LitColumnsApp::OnMouseMove(WPARAM btnState, int x, int y)
     mLastMousePos.y = y;
 }
  
-void LitColumnsApp::OnKeyboardInput(const GameTimer& gt)
+void AmbientLightApp::OnKeyboardInput(const GameTimer& gt)
 {
 }
  
-void LitColumnsApp::UpdateCamera(const GameTimer& gt)
+void AmbientLightApp::UpdateCamera(const GameTimer& gt)
 {
 	// Convert Spherical to Cartesian coordinates.
 	mEyePos.x = mRadius*sinf(mPhi)*cosf(mTheta);
@@ -349,12 +349,12 @@ void LitColumnsApp::UpdateCamera(const GameTimer& gt)
 	XMStoreFloat4x4(&mView, view);
 }
 
-void LitColumnsApp::AnimateMaterials(const GameTimer& gt)
+void AmbientLightApp::AnimateMaterials(const GameTimer& gt)
 {
 	
 }
 
-void LitColumnsApp::UpdateObjectCBs(const GameTimer& gt)
+void AmbientLightApp::UpdateObjectCBs(const GameTimer& gt)
 {
 	auto currObjectCB = mCurrFrameResource->ObjectCB.get();
 	for(auto& e : mAllRitems)
@@ -378,7 +378,7 @@ void LitColumnsApp::UpdateObjectCBs(const GameTimer& gt)
 	}
 }
 
-void LitColumnsApp::UpdateMaterialCBs(const GameTimer& gt)
+void AmbientLightApp::UpdateMaterialCBs(const GameTimer& gt)
 {
 	auto currMaterialCB = mCurrFrameResource->MaterialCB.get();
 	for(auto& e : mMaterials)
@@ -404,7 +404,7 @@ void LitColumnsApp::UpdateMaterialCBs(const GameTimer& gt)
 	}
 }
 
-void LitColumnsApp::UpdateMainPassCB(const GameTimer& gt)
+void AmbientLightApp::UpdateMainPassCB(const GameTimer& gt)
 {
 	XMMATRIX view = XMLoadFloat4x4(&mView);
 	XMMATRIX proj = XMLoadFloat4x4(&mProj);
@@ -439,7 +439,7 @@ void LitColumnsApp::UpdateMainPassCB(const GameTimer& gt)
 	currPassCB->CopyData(0, mMainPassCB);
 }
 
-void LitColumnsApp::BuildRootSignature()
+void AmbientLightApp::BuildRootSignature()
 {
 	// Root parameter can be a table, root descriptor or root constants.
 	CD3DX12_ROOT_PARAMETER slotRootParameter[3];
@@ -471,7 +471,7 @@ void LitColumnsApp::BuildRootSignature()
 		IID_PPV_ARGS(mRootSignature.GetAddressOf())));
 }
 
-void LitColumnsApp::BuildShadersAndInputLayout()
+void AmbientLightApp::BuildShadersAndInputLayout()
 {
 	const D3D_SHADER_MACRO alphaTestDefines[] =
 	{
@@ -490,7 +490,7 @@ void LitColumnsApp::BuildShadersAndInputLayout()
     };
 }
 
-void LitColumnsApp::BuildShapeGeometry()
+void AmbientLightApp::BuildShapeGeometry()
 {
     GeometryGenerator geoGen;
 	GeometryGenerator::MeshData box = geoGen.CreateBox(1.5f, 0.5f, 1.5f, 3);
@@ -610,7 +610,7 @@ void LitColumnsApp::BuildShapeGeometry()
 	mGeometries[geo->Name] = std::move(geo);
 }
 
-void LitColumnsApp::BuildSkullGeometry()
+void AmbientLightApp::BuildSkullGeometry()
 {
 	std::ifstream fin("Models/skull.txt");
 
@@ -685,7 +685,7 @@ void LitColumnsApp::BuildSkullGeometry()
 	mGeometries[geo->Name] = std::move(geo);
 }
 
-void LitColumnsApp::BuildPSOs()
+void AmbientLightApp::BuildPSOs()
 {
     D3D12_GRAPHICS_PIPELINE_STATE_DESC opaquePsoDesc;
 
@@ -718,7 +718,7 @@ void LitColumnsApp::BuildPSOs()
     ThrowIfFailed(md3dDevice->CreateGraphicsPipelineState(&opaquePsoDesc, IID_PPV_ARGS(&mOpaquePSO)));
 }
 
-void LitColumnsApp::BuildFrameResources()
+void AmbientLightApp::BuildFrameResources()
 {
     for(int i = 0; i < gNumFrameResources; ++i)
     {
@@ -727,7 +727,7 @@ void LitColumnsApp::BuildFrameResources()
     }
 }
 
-void LitColumnsApp::BuildMaterials()
+void AmbientLightApp::BuildMaterials()
 {
 	auto bricks0 = std::make_unique<Material>();
 	bricks0->Name = "bricks0";
@@ -767,7 +767,7 @@ void LitColumnsApp::BuildMaterials()
 	mMaterials["skullMat"] = std::move(skullMat);
 }
 
-void LitColumnsApp::BuildRenderItems()
+void AmbientLightApp::BuildRenderItems()
 {
 	auto boxRitem = std::make_unique<RenderItem>();
 	XMStoreFloat4x4(&boxRitem->World, XMMatrixScaling(2.0f, 2.0f, 2.0f)*XMMatrixTranslation(0.0f, 0.5f, 0.0f));
@@ -871,7 +871,7 @@ void LitColumnsApp::BuildRenderItems()
 		mOpaqueRitems.push_back(e.get());
 }
 
-void LitColumnsApp::DrawRenderItems(ID3D12GraphicsCommandList* cmdList, const std::vector<RenderItem*>& ritems)
+void AmbientLightApp::DrawRenderItems(ID3D12GraphicsCommandList* cmdList, const std::vector<RenderItem*>& ritems)
 {
     UINT objCBByteSize = d3dUtil::CalcConstantBufferByteSize(sizeof(ObjectConstants));
     UINT matCBByteSize = d3dUtil::CalcConstantBufferByteSize(sizeof(MaterialConstants));
