4729292016470979e416582e76e5ccd7c97bcd20
PHolmes1011 <61152948+PHolmes1011@users.noreply.github.com>
2021-10-25 18:23:04 +0100

    Removed All Lighting

--- Shaders/Default.hlsl
+++ Shaders/Default.hlsl
@@ -99,21 +99,21 @@ float4 PS(VertexOut pin) : SV_Target
     // Vector from point being lit to eye. 
     float3 toEyeW = normalize(gEyePosW - pin.PosW);
 
-	// Indirect lighting.
-    float4 ambient = gAmbientLight*gDiffuseAlbedo;
+	//// Indirect lighting.
+    //float4 ambient = gAmbientLight*gDiffuseAlbedo;
 
-    const float shininess = 1.0f - gRoughness;
-    Material mat = { gDiffuseAlbedo, gFresnelR0, shininess };
-    float3 shadowFactor = 1.0f;
-    float4 directLight = ComputeLighting(gLights, mat, pin.PosW, 
-        pin.NormalW, toEyeW, shadowFactor);
+    //const float shininess = 1.0f - gRoughness;
+    //Material mat = { gDiffuseAlbedo, gFresnelR0, shininess };
+    //float3 shadowFactor = 1.0f;
+    //float4 directLight = ComputeLighting(gLights, mat, pin.PosW, 
+    //    pin.NormalW, toEyeW, shadowFactor);
 
-    float4 litColor = ambient + directLight;
+    //float4 litColor = ambient + directLight;
 
-    // Common convention to take alpha from diffuse material.
-    litColor.a = gDiffuseAlbedo.a;
+    //// Common convention to take alpha from diffuse material.
+    //litColor.a = gDiffuseAlbedo.a;
 
-    return litColor;
+    return gDiffuseAlbedo;
 }
 
 
